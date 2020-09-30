02981f850855603c01122032f79d4afcfea3da41
Mwansa Muloboka <mwansamuloboka9@gmail.com>
2020-09-30 18:16:52 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+charactersList = documents.getElementById('charactersList');
+const searchBar = document.getElementById("searchBar");
+let hpCharacters = [];
+
+searchBar.addEventListener("keyup", e =>{
+    const searchString = e.target.value.toLowerCase();
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.includes(searchString) ||
+            character.house.includes(searchString)
+            );
+  });
+  displayCharacters(filteredCharacters);      
+});
+
+const loadCharacters = async () => {
+    try{
+        const res= await fetch('http://hp-ip.herokuapp.com/api/characters');
+        hpCharacters = await res.json()
+        displayCharacters(hpCharacters);
+    }catch(err){
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => { 
+    const htmlString = characters.map((character) => {
+        return 
+        <li class="character">
+            <h2>${character.name}</h2>
+            <p>House: ${ character.house}</p>
+            <img src="${character.image}"></img>
+        </li>
+    })
+    .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
+
+  
